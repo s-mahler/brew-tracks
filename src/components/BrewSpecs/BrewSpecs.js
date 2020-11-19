fae260ffd66137cddd1fbeb8bf905c470885d4f9
Sam Mahler <mahler.sam@gmail.com>
2020-11-19 16:41:01 -0600

    form styling for specs and tasting notes

--- src/components/BrewSpecs/BrewSpecs.js
+++ src/components/BrewSpecs/BrewSpecs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import './BrewSpecs.css'
 
 class BrewSpecs extends Component {
 
@@ -10,7 +11,7 @@ class BrewSpecs extends Component {
 
     render() {
         return (
-            <div className="formPanel">
+            <div className="formPanel specs">
                 <h1>Brew Specifications</h1>
 
                 <label>Method:</label>
