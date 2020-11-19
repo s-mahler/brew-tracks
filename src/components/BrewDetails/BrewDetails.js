baba36bb3a31a099683f82042af26fb45e99c275
Sam Mahler <mahler.sam@gmail.com>
2020-11-19 15:13:46 -0600

    toggle function

--- src/components/BrewDetails/BrewDetails.js
+++ src/components/BrewDetails/BrewDetails.js
@@ -4,6 +4,13 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class BrewDetails extends Component {
 
+    // TO DO STILL
+    // GET for times
+
+    state = {
+        toggle: true
+    }
+
     goBack = () => {
         this.props.history.push(`/brews/${this.props.store.user.id}`)
     }
@@ -16,32 +23,47 @@ class BrewDetails extends Component {
         this.props.dispatch({type: 'GET_SPECIFIC_BREW', payload: this.props.match.params.id});
     }
 
+    toggleView = () => {
+        this.setState({
+            toggle: !this.state.toggle,
+        });
+    }
+
     render() {
         return (
             <>
-                <button>Edit</button>
+                <button onClick={this.toggleView}>Edit</button>
                 <h1>Brew Details</h1>
-                <p>Will be populated from DB</p>
+
+                {this.state.toggle ? (
+
+                <>
                 {this.props.store.brew.map(brew => {
-                        return <div onClick={() => this.goToDetails(brew.id)}>
-                                    <div className="specs">
-                                        <p>{brew.brew_method}</p>
-                                        <p>{brew.origin}</p>
-                                        <p>{brew.roast}</p>
-                                        <p>{brew.grind}</p>
-                                        <p>{brew.coffee_amount}</p>
-                                        <p>{brew.water_amount}</p>
-                                    </div>
-
-                                    <div>
-                                        <p>{brew.aroma}</p>
-                                        <p>{brew.taste}</p>
-                                        <p>{brew.body}</p>
-                                        <p>{brew.mouth_feel}</p>
-                                    </div>
+                    return <div key={brew.id} onClick={() => this.goToDetails(brew.id)}>
+                                <div className="specs">
+                                    <p>{brew.brew_method}</p>
+                                    <p>{brew.origin}</p>
+                                    <p>{brew.roast}</p>
+                                    <p>{brew.grind}</p>
+                                    <p>{brew.coffee_amount}</p>
+                                    <p>{brew.water_amount}</p>
                                 </div>
-                    })}
 
+                                <div>
+                                    <p>{brew.aroma}</p>
+                                    <p>{brew.taste}</p>
+                                    <p>{brew.body}</p>
+                                    <p>{brew.mouth_feel}</p>
+                                </div>
+                            </div>
+                    })}
+                    </>
+
+                ) : (
+
+                    <p>the other</p>
+
+                )}
                 <button onClick={this.goBack}>Back</button>
             </>
         )
