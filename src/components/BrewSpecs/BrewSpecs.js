a588990272290b9053733d4bf9a780d0f8c891fe
Sam Mahler <mahler.sam@gmail.com>
2020-11-19 16:42:49 -0600

    removed unnecessary file

--- src/components/BrewSpecs/BrewSpecs.js
+++ src/components/BrewSpecs/BrewSpecs.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import './BrewSpecs.css'
 
 class BrewSpecs extends Component {
 
