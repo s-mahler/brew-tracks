b654bbc66bb5060ea485e580ee06ac786ffd1b49
Sam Mahler <mahler.sam@gmail.com>
2020-11-19 15:54:13 -0600

    flex styling for edit form

--- src/components/BrewDetails/BrewDetails.js
+++ src/components/BrewDetails/BrewDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import './BrewDetails.css';
 
 class BrewDetails extends Component {
 
@@ -32,27 +33,46 @@ class BrewDetails extends Component {
     render() {
         return (
             <>
-                <button onClick={this.toggleView}>Edit</button>
-                <h1>Brew Details</h1>
+
 
                 {this.state.toggle ? (
 
                 <>
+                <button onClick={this.toggleView}>Edit</button>
+                <h1>Brew Details</h1>
                 {this.props.store.brew.map(brew => {
-                    return <div key={brew.id} onClick={() => this.goToDetails(brew.id)}>
+                    return <div key={brew.id}>
                                 <div className="specs">
+                                    <label>Method:</label>
                                     <p>{brew.brew_method}</p>
+
+                                    <label>Origin:</label>
                                     <p>{brew.origin}</p>
+
+                                    <label>Roast:</label>
                                     <p>{brew.roast}</p>
+
+                                    <label>Grind:</label>
                                     <p>{brew.grind}</p>
+
+                                    <label>Amount of Coffee:</label>
                                     <p>{brew.coffee_amount}</p>
+
+                                    <label>Amount of Water:</label>
                                     <p>{brew.water_amount}</p>
                                 </div>
 
-                                <div>
+                                <div className="tasting">
+                                    <label>Aroma:</label>
                                     <p>{brew.aroma}</p>
+
+                                    <label>Taste:</label>
                                     <p>{brew.taste}</p>
+
+                                    <label>Body:</label>
                                     <p>{brew.body}</p>
+
+                                    <label>Mouth Feel:</label>
                                     <p>{brew.mouth_feel}</p>
                                 </div>
                             </div>
@@ -61,7 +81,43 @@ class BrewDetails extends Component {
 
                 ) : (
 
-                    <p>the other</p>
+                    <div>
+                        <button onClick={this.toggleView}>Save</button>
+                        <h1>Brew Details</h1>
+                        <div className="specs">
+                                    <label>Method:</label>
+                                    <input></input>
+
+                                    <label>Origin:</label>
+                                    <input></input>
+
+                                    <label>Roast:</label>
+                                    <input></input>
+
+                                    <label>Grind:</label>
+                                    <input></input>
+
+                                    <label>Amount of Coffee:</label>
+                                    <input></input>
+
+                                    <label>Amount of Water:</label>
+                                    <input></input>
+                                </div>
+
+                                <div className="tasting">
+                                    <label>Aroma:</label>
+                                    <input></input>
+
+                                    <label>Taste:</label>
+                                    <input></input>
+
+                                    <label>Body:</label>
+                                    <input></input>
+
+                                    <label>Mouth Feel:</label>
+                                    <input></input>
+                                </div>
+                    </div>
 
                 )}
                 <button onClick={this.goBack}>Back</button>
